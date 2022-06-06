f78fb1ac990c40c16b10ab5b436bae0601e12682
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-06-06 19:02:07 +0530

    n=int(input()) a=list(map(int,input().split())) m=min(a) import math l=int(math.log10(m))+1 c=0 for i in a:     len=int(math.log10(i))+1     if len==l:         c+=1 print(c)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,11 @@
+n=int(input())
+a=list(map(int,input().split()))
+m=min(a)
+import math
+l=int(math.log10(m))+1
+c=0
+for i in a:
+    len=int(math.log10(i))+1
+    if len==l:
+        c+=1
+print(c)
