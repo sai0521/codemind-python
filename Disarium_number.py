543e2b64f93dd87a8379db70e257703a671b5950
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-07-02 06:29:34 +0530

    n=int(input()) m=n import math c=int(math.log10(n))+1 s=0 while m:     r=m%10     m=m//10     s+=pow(r,c)     c-=1 if s==n:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,14 +1,14 @@
-a=int(input())
-n=a
+n=int(input())
+m=n
 import math
-l=int(math.log10(n))+1
+c=int(math.log10(n))+1
 s=0
-while n:
-    m=n%10
-    n=n//10
-    s+=pow(m,l)
-    l-=1
-if s==a:
+while m:
+    r=m%10
+    m=m//10
+    s+=pow(r,c)
+    c-=1
+if s==n:
     print("True")
 else:
     print("False")
