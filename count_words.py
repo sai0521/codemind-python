1bf96d7d60f59beed8645bb549aba3b350d9f110
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-10-15 08:18:46 +0530

    n=input() n=n.split(" ") c=0 for i in n:     if i[0] in "aeiouAEIOU" and i[-1] not in "aeiouAEIOU":         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,6 +1,7 @@
-n=input().split()
+n=input()
+n=n.split(" ")
 c=0
 for i in n:
-    if i[0] in "AEIOUaeiou" and i[-1] not in "aeiouAEIOU":
+    if i[0] in "aeiouAEIOU" and i[-1] not in "aeiouAEIOU":
         c+=1
 print(c)
