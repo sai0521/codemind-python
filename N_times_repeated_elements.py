6f0452cd8eecb44db27ff23c1e21f5d7162c28e7
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-10-15 22:59:47 +0530

    n=int(input()) l=list(map(int,input().split())) m=int(input()) s=[] l1=[] for i in l:     if i not in l1:         l1.append(i) for i in l1:     if m==l.count(i):         s.append(i) if s==[]:     print(-1) else:     print(*s)

--- N_times_repeated_elements.py
+++ N_times_repeated_elements.py
@@ -2,8 +2,12 @@ n=int(input())
 l=list(map(int,input().split()))
 m=int(input())
 s=[]
+l1=[]
 for i in l:
-    if l.count(i)==m and i not in s:
+    if i not in l1:
+        l1.append(i)
+for i in l1:
+    if m==l.count(i):
         s.append(i)
 if s==[]:
     print(-1)
