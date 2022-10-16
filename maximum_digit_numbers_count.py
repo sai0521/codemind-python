3de2108246bf91920c7d847693e709a615a21227
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-10-16 10:51:35 +0530

    import math n=int(input()) l=list(map(int,input().split())) d=0 for i in l:     c=len(str(i))     if c>d:         d=c for i in l:     if len(str(i)) == d:         print(i,end=" ")

--- maximum_digit_numbers_count.py
+++ maximum_digit_numbers_count.py
@@ -1,20 +1,11 @@
-def count(x):
-    c=0
-    if x<0:
-        x=-(x)
-    while x:
-        x=x//10
-        c+=1
-    return c
-
+import math
 n=int(input())
 l=list(map(int,input().split()))
-a=[]
+d=0
 for i in l:
-    a.append(count(i))
-c=max(a)
-s=[]
+    c=len(str(i))
+    if c>d:
+        d=c
 for i in l:
-    if c==count(i) and i not in s:
-        s.append(i)
-print(*s)
+    if len(str(i)) == d:
+        print(i,end=" ")
