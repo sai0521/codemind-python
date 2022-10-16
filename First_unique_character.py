05551113dde758a8df2db1b1ddfbd58d20b937e4
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-10-16 11:17:21 +0530

    n=input() n=n.lower() s=[] for i in n:     if i!=" " and n.count(i)==1:         s.append(i) if s==[]:     print(-1) else:     print(s[0])

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,9 +1,10 @@
 n=input()
-c=0
+n=n.lower()
+s=[]
 for i in n:
     if i!=" " and n.count(i)==1:
-        print(i)
-        c+=1
-        break
-if c==0:
+        s.append(i)
+if s==[]:
     print(-1)
+else:
+    print(s[0])
